235b3c6bade964b1e0a5426c06f174889309acac
jbinnema <jbinnema@unbc.ca>
2021-03-23 02:27:16 +0000

    Beginnings of the C file. Complete header file.

---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef CFILE_H_
+#define CFILE_H_
+
+int subtract (int a, int b);
+int addition (int a, int b);
+int exponent (int a, int b);
+int divide (int a, int b);
+int bitcount (int a);
+int sum (int a, int b);
+int factorial (int a);
+
+#endif //Cfile_H_
+
