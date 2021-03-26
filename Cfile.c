b2cd9d3c867677b94d6ec90a6c0baaedade0883e
jbinnema <jbinnema@unbc.ca>
2021-03-26 19:50:31 +0000

    initial work on Cfile, bug testing for some .S and included the forgotten multiply.S

--- Cfile.c
+++ Cfile.c
@@ -2,7 +2,33 @@
 #include "header.h"
 int main(void){
 
+	char char1;
+	char char2;
+	char char3;
+	int int1;
+	int int2;
 
 
-return 0
+	printf("Hello! I am Josiah's Computational Calcutron. \nI am capable of a few strange mathematical operations involving integers. Try me! \n");
+	printf("Type \"help\" for a list of commands, or type a command to get started! Type \"quit\" at any time to exit the program \n");
+	scanf(
+
+
+
+		printf("\nCommands must have at least the first three characters typed correctly \n\n");
+		printf("help 		- this is what you typed to get this list\n");
+		printf("quit 		- exit the program\n");
+		printf("add 		- add the next two digits\n");
+		printf("subtract 	- subtract the next two integers\n");
+		printf("multiply 	- multiply the next two integers\n");
+		printf("exponent 	- raises the first integer to the power of the second integer\n");
+		printf("divide		- divides the first integer by the second integer, then truncates the result\n");
+		printf("bitcount 	- returns the number of set bits in the binary representation of the integer\n");
+		printf("sum 		- sums all numbers between the two integer entered (inclusive)\n");
+		printf("factorial	- computes the factorial of the integer\n\n");
+
+//use a compare to ensure the sum function works correctly
+
+
+return 0;
 }
